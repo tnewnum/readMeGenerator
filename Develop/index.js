1a2195172a903df67c799e6fa658232d6deb24f1
Thomas Newnum <thomas.newnum@gmail.com>
2023-01-28 16:50:20 -0500

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+      },
+      {
+        type: 'password',
+        message: 'What is your password?',
+        name: 'password',
+      },
+      {
+        type: 'password',
+        message: 'Re-enter password to confirm:',
+        name: 'confirm',
+      },
+];
+
+// TODO: Create a function to write README file
+function writeToFile('README.md', 'utf8', (error, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
